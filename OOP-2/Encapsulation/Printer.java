aaa31ad0e1f2bf72adb51ab4cfd01eedbf8bcfac
Ugur <ercinugur@gmail.com>
2022-02-07 22:21:02 +0300

    initial commit

---
+++ OOP-2/Encapsulation/Printer.java
@@ -0,0 +1,40 @@
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        if(tonerLevel > -1 && tonerLevel <= 100) {
+            this.tonerLevel = tonerLevel;
+        } else {
+            tonerLevel = -1;
+        }
+        this.duplex = duplex;
+        this.pagesPrinted = 0;
+    }
+
+    public int addToner(int tonerAmount) {
+        if(tonerAmount > 0 && tonerAmount <= 100) {
+            if((tonerAmount + this.tonerLevel) > 100) {
+                return -1;
+            }
+            return -1;
+        }
+        return this.tonerLevel += tonerAmount;
+    }
+
+    public int printPages(int pages) {
+        int pagesToPrint = pages;
+        if(this.duplex) {
+            pagesToPrint /= 2;
+            System.out.println("Printing in dublex mode");
+        }
+        this.pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+
+    public int getPagesPrinted() {
+        return this.pagesPrinted;
+    }
+
+}
